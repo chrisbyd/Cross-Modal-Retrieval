d05e592d43a226b425a6a6db40ee0f2c67efebb8
chrisbyd <chenyongbiao0319@sina.com>
2021-07-11 10:21:10 +0800

    another uniformer with bert and vit

--- networks/uniformer.py
+++ networks/uniformer.py
@@ -13,21 +13,46 @@ import torch.nn as nn
 from .New_vision_transformer import Block as VBlock
 import utils.checkpoint as checkpoint
 from utils import np2th
+from pytorch_transformers import BertTokenizer, BertModel, BertForMaskedLM, BertConfig
 
 class Uniformer(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
-        self.textformer = TextFormer(config)
+        modelConfig = BertConfig.from_pretrained('./pretrained_dir/bert_pretrain/bert-base-uncased-config.json')
+        self.textformer = BertModel.from_pretrained(
+            './pretrained_dir/bert_pretrain/bert-base-uncased-pytorch_model.bin', config=modelConfig)
+       # self.textformer = TextFormer(config)
         self.visionformer = VisionFormer(config,config["crop_size"], False)
         self.c_blocks = nn.ModuleList([VBlock(config, False)  for _ in range(self.config['n_common_layers'])])
         self.text_hash_layer = Linear(self.config["dim"], self.config["hash_length"])
         self.image_hash_layer = Linear(self.config["dim"], self.config["hash_length"])
         self.tanh = nn.Tanh()
+        self.initialize_common_params()
+
+    def weights_init_kaiming(self, m):
+        classname = m.__class__.__name__
+        if classname.find('Linear') != -1:
+            nn.init.kaiming_normal_(m.weight, a=0, mode='fan_out')
+            nn.init.constant_(m.bias, 0.0)
+
+        elif classname.find('Conv') != -1:
+            nn.init.kaiming_normal_(m.weight, a=0, mode='fan_in')
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0.0)
+        elif classname.find('BatchNorm') != -1:
+            if m.affine:
+                nn.init.constant_(m.weight, 1.0)
+                nn.init.constant_(m.bias, 0.0)
+
+    def initialize_common_params(self):
+        for block in self.c_blocks:
+            block.apply(self.weights_init_kaiming)
+
 
 
     def forward_text(self, x, segs, mask):
-        h = self.textformer(x, segs, mask)
+        h, _ = self.textformer(x, segs, mask)
         for block in self.c_blocks:
             h,_ = block(h)
         h = self.text_hash_layer(h[:, 0])
@@ -69,9 +94,9 @@ class Uniformer(nn.Module):
         text_weight_path = pretrained_file['text']
         img_weight_path  = pretrained_file['vision']
         print("Starting loading pretrained text transformer ")
-        if text_weight_path.endswith('.ckpt'):  # checkpoint file in tensorflow
-            print("Start loading ckpt")
-            checkpoint.load_model(self.textformer, text_weight_path)
+        # if text_weight_path.endswith('.ckpt'):  # checkpoint file in tensorflow
+        #     print("Start loading ckpt")
+        #     checkpoint.load_model(self.textformer, text_weight_path)
 
         print("Starting loading pretrained image transformer")
         self.load_vision_from(img_weight_path)
